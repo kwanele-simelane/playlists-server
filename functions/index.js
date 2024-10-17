23d99f6b70ee616be91d96d2506f3371b1a32c34
Kwanele Simelane <83864710+kwanele-simelane@users.noreply.github.com>
2024-10-17 16:31:57 +0200

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,1 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
+require("dotenv").config();
+
+const authRoutes = require("./routes/auth");
+const playlistRoutes = require("./routes/playlist");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/", authRoutes);
+app.use("/playlists", playlistRoutes);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
